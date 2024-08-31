8273004543ee320c93b8ec0e81cb0e26fce81bcd
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2024-08-31 17:55:16 +0530

    Update WeatherRow.jsx

--- Week3/_28_lecture_Project/src/components/WeatherRow.jsx
+++ Week3/_28_lecture_Project/src/components/WeatherRow.jsx
@@ -4,7 +4,7 @@ function WeatherRow() {
   return (
     <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700  ">
       <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-        Apple MacBook Pro 17"
+       Lenovo IdeaPad Slim 3"
       </th>
       <td className="px-6 py-4">
         Silver
